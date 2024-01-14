687059d92a232e1d1486b6afff2b220da006ec24
Sushil Hemrom <sushil500530@gmail.com>
2024-01-14 16:38:07 +0600

    add get all data

--- index.js
+++ index.js
@@ -8,36 +8,40 @@ const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@clust
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // app.get("/", async(req, res) => {
-    //     try{
-    
-    //     }
-    //     catch(error){
-    //         console.log(error);
-    //     }
-    // })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    const cloudDataCollection = await client.db('cloudDB').collection("clout_datas");
+
+    try {
+        app.get("/cloud-datas", async (req, res) => {
+            try {
+                const result = await cloudDataCollection.find().toArray();
+                res.send(result)
+            }
+            catch (error) {
+                console.log(error);
+            }
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is Running Now.....');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is Running on port ${port}`);
 })
